20793476431ce2bae56ed5731bfa25e5e62d0262
mwangiKibui <kenmwangi250@gmai.com>
2020-09-10 13:05:15 +0300

    fixing the login section

--- src/App.js
+++ src/App.js
@@ -13,25 +13,18 @@ import Register from './views/signup';
 //styles
 import './scss/style.scss';
 
-const loading = (
-  <div className="pt-3 text-center">
-    <div className="sk-spinner sk-spinner-pulse"></div>
-  </div>
-);
 
 class App extends Component {
 
   render() {
     return (
       <Router>
-          <React.Suspense fallback={loading}>
             <Switch>
               <Route exact path="/auth/login" name="Login" component={Login} />
               <Route exact path="/auth/signup" name="Register" component={Register}/>
               <Route exact path="/auth/logout" name="Logout" component={Logout} />
               <Route path="/" name="Home" component={Layout} />
             </Switch>
-          </React.Suspense>
       </Router>
     );
   }
